3f176257ac72d8fd44d4e419a83d4a6931665c2c
Ramin Joshang <91675032+Ramin-Joshang@users.noreply.github.com>
2025-05-28 12:47:35 +0330

    paint updated

--- Paint-Final/include/transform.h
+++
@@ -1,9 +0,0 @@
-// transform.h
-#pragma once
-
-#include "shape.h"
-
-void translatePoint(float x, float y);
-void scalePoint(float x, float y);
-void rotationPoint(float x, float y);
-bool IsAbove(float x, float y);
--- paint-2/paint2.cpp
+++
@@ -1,6 +0,0 @@
-#include "paint2.h"
-
-paint2::paint2(QWidget *parent)
-    : QWidget(parent)
-{
-}
